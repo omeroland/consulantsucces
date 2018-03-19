b54c7999809347aad8815649c183f7b532e5f9ce
omeroland <omeroland@omeroland.net>
2018-03-19 18:43:27 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+
+app.use('/', express.static(__dirname + '/public'));
+
+app.listen(process.env.PORT  || 3000, function () {
+    console.log('Server is start');
+});
